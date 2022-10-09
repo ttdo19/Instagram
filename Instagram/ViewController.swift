9666bb0521ced23c8fe9c5920d5fc659b262e3a4
ttdo19 <thuytrang19072001@gmail.com>
2022-10-09 16:59:44 -0400

    First commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Trang Do on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
